a3da3e744d46267a3dc2e0df54b8dffe9709ca4b
Francisco Rafart <francisco.rafart@gmail.com>
2018-11-06 18:52:14 -0500

    clean index.js

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,6 +1,4 @@
 import { combineReducers } from 'redux';
 import initGame from './initGame';
-// import playGame from './playGame';
 
 export default combineReducers({ initGame });
-// export default combineReducers({ initGame, playGame });
