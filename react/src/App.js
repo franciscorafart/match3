6acb48e0ad6e7051b8e437a2006f4bde6b8bc017
Dunia Goncalves <dgoncalves@everquote.com>
2019-02-06 23:53:19 -0500

    Adding swagger

--- react/src/App.js
+++ react/src/App.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
+import './App.css';
 import Level from './components/Level';
+import SwaggerUI from 'swagger-ui';
 
-export default class App extends Component {
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      definitionLink: "https://petstore.swagger.io/v2/swagger.json",
+    }
+  }
+
+  componentDidMount() {
+    SwaggerUI({
+      domNode: document.getElementById("api-data"),
+      url: this.state.definitionLink
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <Level />
+        <Level/>
+        <div id="api-data" />
       </div>
     );
   }
